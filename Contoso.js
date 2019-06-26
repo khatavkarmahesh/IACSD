a95f2f2bd99cddebeb81eb6c9c86f83c849e6c04
dac <dac@dypm.org>
2019-06-26 07:17:15 +0530

    Initial commit

---
+++ Contoso.js
@@ -0,0 +1,21 @@
+var http=require("http");
+var express= require("express");
+console.log("Hello World");
+var app=express();
+
+
+
+var Accountcontroller=function(req,resp){
+var customer =[{customer:"Oracle",location:"England",turnover:10},
+{customer:"MRF-Tyre",location:"India",turnover:20},
+{customer:"AXIS-Bank",location:"Africa",turnover:30}];
+resp.send(customer);
+}
+
+app.get('/',Accountcontroller);
+
+var server=app.listen(8000,function(){
+var host =server.address().address;
+var port =server.address().port;
+console.log("Example app listening at http://localhost:8000", host, port);
+})
